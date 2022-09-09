f62940f2cffd3ba4a5682c6f725f2c821eced372
andreluisklr <84353374+andreluisklr@users.noreply.github.com>
2022-09-09 20:24:28 -0300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-import { getUser } from '/src/scripts/services/users.js'
-import { getRepositories } from '/src/scripts/services/repositories.js'
+import { getUser } from 'testando/src/scripts/services/users.js'
+import { getRepositories } from 'testando/src/scripts/services/repositories.js'
 
-import { user } from '/src/scripts/objects/user.js'
-import { screen } from '/src/scripts/objects/screen.js'
+import { user } from 'testando/src/scripts/objects/user.js'
+import { screen } from 'testando/src/scripts/objects/screen.js'
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
